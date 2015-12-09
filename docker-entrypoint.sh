0ec695bb97c05d2fe8faed808b4d6f8cee2980bc
Brian Winter <Brian.Winter@nuance.com>
2015-12-09 20:40:11 +0000

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'cache' ]; then
+	chmod 777 /carbon_cache_relay_init_script.sh
+    ./carbon_cache_relay_init_script.sh
+    exec /usr/bin/carbon-cache --config=/etc/carbon/carbon.conf --debug start "$@"
+elif [ "$1" = 'relay' ]; then
+	chmod 777 /carbon_cache_relay_init_script.sh
+    ./carbon_cache_relay_init_script.sh
+    exec /usr/bin/carbon-relay --config=/etc/carbon/carbon.conf --debug start "$@"
+elif [ "$1" = 'whisper' ]; then
+    exec echo "Starting Whisper Data Container..."
+fi
+
+exec "$@"
